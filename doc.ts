f8bc9a843df1d34da0cb49569e5530f28dd75658
deller <dennis.eller@fh-bielefeld.de>
2021-05-14 19:30:20 +0200

    add2

--- doc.ts
+++ doc.ts
@@ -21,7 +21,15 @@
  export function add(x: number, y: number): number {
     return x + y;
   }
-
+/**
+ * Adds x and y.
+ * @param {number} x
+ * @param {number} y
+ * @returns {number} Sum of x and y
+ */
+ export function add2(x: number, y: number): number {
+    return x + y;
+  }
  
 
 
