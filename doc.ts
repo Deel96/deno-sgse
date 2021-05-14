48f3bde829145e85401600d1fb2d80059cbb3294
deller <dennis.eller@fh-bielefeld.de>
2021-05-14 19:36:03 +0200

    test

--- doc.ts
+++ doc.ts
@@ -33,6 +33,16 @@
  
 
 
+  /**
+ * Adds x and y.
+ * @param {number} x
+ * @param {number} y
+ * @returns {number} Sum of x and y
+ */
+ export function add2s(x: number, y: number): number {
+    return x + y;
+  }
+
 
   const test = new ImportantClass("John");
   console.log(test);
